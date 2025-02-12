29fa6631244f58fc6c2dd85f3c4fa49e7594ed8a
harfool gujjar <=>
2025-02-12 06:28:01 +0530

    ai Integration

---
+++ services/aiService.js
@@ -0,0 +1,18 @@
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+
+const apiKey = process.env.GOOGLE_API_KEY 
+const genAI = new GoogleGenerativeAI(apiKey);
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+
+const prompt = "Explain how AI works";
+
+const result = await model.generateContent(prompt);
+console.log(result.response.text());
+
+const generateContent = async (prompt)=>{
+const result = await model.generateContent(prompt)
+return result.response.text()
+}
+
+export default generateContent
