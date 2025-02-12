d33134ed6a8b182b8ae30959c24e3e25d30e678f
harfool gujjar <=>
2025-02-12 06:20:02 +0530

    ai intergration

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import app from "./app.js";
+import dotenv from "dotenv"
+dotenv.config({path : "./.env"})
 
+const Port = process.env.PORT || 8000
 app.listen(3000 , ()=>{
-    console.log(`server run on 3000`)
+    console.log(`server run on ${Port}`)
 })
